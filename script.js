9d3d85c109add4bd2bfd68f5bdd4698c77678cae
theWebCookie <jabwojtek@wp.pl>
2024-02-12 22:44:10 +0100

    refactor: improve soundwave animation

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 const soundWaveBox = document.querySelector('.soundWave');
 const fileInput = document.getElementById('audioInput');
 const audioPlayer = document.getElementById('audioPlayer');
-const audioContext = new (window.AudioContext || window.AudioContext)();
-const analyser = audioContext.createAnalyser();
-const source = audioContext.createMediaElementSource(audioPlayer);
-
-source.connect(analyser);
-analyser.connect(audioContext.destination);
+let audioContext;
+let analyser;
+let isPlaying = false;
+let now, then, elapsed, fpsInterval;
 
 const dictionary = {
   gridRows: 6,
@@ -15,7 +13,6 @@ const dictionary = {
 
 const { gridRows, gridCols } = dictionary;
 
-// Display Grid
 for (let row = 0; row < gridRows; row++) {
   const tr = document.createElement('tr');
   for (let col = 0; col < gridCols; col++) {
@@ -25,7 +22,6 @@ for (let row = 0; row < gridRows; row++) {
   soundWaveBox.appendChild(tr);
 }
 
-// Get Input
 fileInput.addEventListener('change', (event) => {
   const fileInput = event.target;
 
@@ -33,28 +29,63 @@ fileInput.addEventListener('change', (event) => {
     const audioFile = URL.createObjectURL(fileInput.files[0]);
     audioPlayer.src = audioFile;
     audioPlayer.play();
+
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    analyser = audioContext.createAnalyser();
+    const source = audioContext.createMediaElementSource(audioPlayer);
+
+    source.connect(analyser);
+    analyser.connect(audioContext.destination);
   }
 });
 
-audioPlayer.addEventListener('timeupdate', function () {
+function animate() {
+  if (!analyser || !isPlaying) return;
+
+  requestAnimationFrame(animate);
+
+  now = Date.now();
+  elapsed = now - then;
+
+  analyser.fftSize = 2048;
   const data = new Uint8Array(analyser.fftSize);
+  let data2 = [];
+
   analyser.getByteTimeDomainData(data);
 
-  for (let col = 0; col < gridCols; col++) {
-    const colIndex = Math.floor((col / gridCols) * data.length);
-    const amplitude = data[colIndex] / 256;
+  if (elapsed > fpsInterval) {
+    then = now - (elapsed % fpsInterval);
+    for (let i = 0; i < gridCols; i++) {
+      data2.push(data[Math.floor((i * analyser.fftSize) / gridCols)]);
+    }
 
-    // Number of cells to color
-    const numColoredCells = Math.floor(amplitude * gridRows);
+    for (let col = 0; col < gridCols; col++) {
+      const colIndex = Math.floor((col / gridCols) * data2.length);
+      const amplitude = ((data2[colIndex] / 255 - 0.4) * 100) / 3;
 
-    for (let row = gridRows - 1; row >= 0; row--) {
-      const td = soundWaveBox.rows[row].cells[col];
+      const numColoredCells = Math.round(amplitude);
 
-      if (gridRows - row <= numColoredCells) {
-        td.classList.add('active');
-      } else {
-        td.classList.remove('active');
+      for (let row = gridRows - 1; row >= 0; row--) {
+        const td = soundWaveBox.rows[row].cells[col];
+        const isActive = gridRows - row <= numColoredCells;
+        td.classList.toggle('active', isActive);
       }
     }
   }
+}
+
+function startAnimating(fps) {
+  fpsInterval = 1000 / fps;
+  then = Date.now();
+  startTime = then;
+  animate();
+}
+
+audioPlayer.addEventListener('play', function () {
+  isPlaying = true;
+  startAnimating(24);
+});
+
+audioPlayer.addEventListener('pause', function () {
+  isPlaying = false;
 });
